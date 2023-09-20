53897d963ea4c0808afc58ed455b1ff849157097
waleed <waleedbakar@outlook.com>
2023-09-20 09:55:06 +0100

    New

--- tests/test_string_builder.py
+++ tests/test_string_builder.py
@@ -1,5 +1,5 @@
 from lib.string_builder import StringBuilder
 
 def test_building_a_string():
-    string = StringBuilder()
+    string_builder = StringBuilder()
     assert string_builder.output() == ""
