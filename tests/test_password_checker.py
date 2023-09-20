636929929e001f51ead7cebc9f50b4e7fb27ffe9
waleed <waleedbakar@outlook.com>
2023-09-20 15:32:43 +0100

    challenge.

--- tests/test_password_checker.py
+++ tests/test_password_checker.py
@@ -3,14 +3,14 @@ from lib.password_checker import PasswordChecker
 
 def test_if_password_is_valid():
     checker = PasswordChecker()
-    checker.len = 8
-    assert checker.len == 8
+    checker.check = 8
+    assert checker.check == 8
 
 def test_if_password_is_invalid():
     checker = PasswordChecker()
     
     with pytest.raises(Exception) as e:
-        checker.wrap()
+        checker.check("passw")
         message = str(e.value)
         assert message == "Invalid password, must be 8+ characters."
 
